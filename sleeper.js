a89c118e006836cf9a41e1baf4e5f317c8cdcd2d
Ian Hansen <ian@supershabam.com>
2013-07-10 16:32:15 -0700

    adds gitignore

--- sleeper.js
+++
@@ -1,9 +0,0 @@
-process.title = 'orphan'
-console.log('sleeper')
-setInterval(function() {
-  console.log('zzzz')
-}, 1000)
-setTimeout(function() {
-  console.log('i slept')
-  process.exit()
-}, 10000)
