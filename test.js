b7c03a5294e739df288b71c7dd5339ec06c1a255
Ian Hansen <ian@supershabam.com>
2013-07-10 22:10:57 -0700

    working

--- test.js
+++ test.js
@@ -5,9 +5,17 @@ var
 orphanage.open(path.resolve('./run'), function(err, orphans) {
   console.log(arguments)
   orphans.on('error', function(err) {
-    logger.error('error', err)
+    console.error('error', err)
   })
-  orphans.spawn('ls', ['-lh', '/usdfsr'], {})
+  orphans.spawn('ls', ['-lh', '/usr'], {respond: '1234'})
   console.log('exit')
-  process.exit()
+  orphans.on('stderr', function() {
+    console.log('stderr', arguments)
+  })
+  orphans.on('complete', function() {
+    console.log('complete', arguments)
+  })
+  orphans.on('stdout', function() {
+    console.log('stdout', arguments)
+  })
 })
